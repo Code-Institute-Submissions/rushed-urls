eb625c9394bab6a380fbcb7222edb3ed817d80c5
Debugged Moon <predojevic@proton.me>
2023-03-10 11:12:27 +0000

    🔒 Add env variable getters to settings.py

--- rushed_urls/settings.py
+++ rushed_urls/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2$o7#h7z$-cfg4vnea#a2#*lynu3+d+-1i5h_ntlekn_&57(z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
