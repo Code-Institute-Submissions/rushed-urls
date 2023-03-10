b8df540351c6a83df39edee47b32789153dc1d39
Debugged Moon <predojevic@proton.me>
2023-03-10 11:49:36 +0000

    🚧 Add temporary hero content and route home

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+
+    View for homepage.
+
+    route: '/'
+    template: 'urlshortener/home.html'
+
+"""
+def get_home(request):
+    return render(request, 'urlshortener/home.html')
